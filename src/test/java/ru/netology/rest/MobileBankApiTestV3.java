848a64e198b03b6fda10dcbf6c3cf45d714d5a5f
AnastasiyaMaksimovna <sturochka98@gmail.com>
2023-03-08 22:53:20 +0500

    Change commit 4

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -1,12 +1,13 @@
-package java.ru.netology.rest;
+package ru.netology.rest;
 
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
-class MobileBankApiTestV5 {
+class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
       // Given - When - Then
@@ -19,8 +20,11 @@ class MobileBankApiTestV5 {
       // Проверки
       .then()
           .statusCode(200)
+          // специализированные проверки - лучше
           .contentType(ContentType.JSON)
-          .body("every{ it.balance >= 0 }", is(true))
+          .body("", hasSize(3))
+          .body("[0].currency", equalTo("RUB"))
+          .body("[0].balance", greaterThanOrEqualTo(0))
       ;
     }
 }
